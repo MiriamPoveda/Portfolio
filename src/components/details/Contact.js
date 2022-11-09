aca9c446ba4473e4525502f7e1b404b0c42f5794
MiriamPoveda <mpoveda94@gmail.com>
2022-11-09 14:31:05 +0100

    Start of contact design

--- src/components/details/Contact.js
+++ src/components/details/Contact.js
@@ -2,13 +2,53 @@
 import Footer from "../global/Footer";
 /* Styles */
 import "../../styles/details/Contact.scss";
+/* Multimedia */
+import CV from "../../multimedia/MiriamPoveda.pdf";
 
 function Contact() {
   return (
     <>
-      <div>
-        <h2>CONTACTO</h2>
-      </div>
+      <main className="contactMain">
+        <div>
+          <a
+            href="https://github.com/MiriamPoveda"
+            title="GitHub"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-brands fa-github-alt"></i>
+          </a>
+          <a
+            href="https://es.linkedin.com/in/miriampoveda"
+            title="Linkedin"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a
+            href="mailto:mpoveda94@gmail.com"
+            title="Email"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+          <a
+            href={CV}
+            download="MiriamPoveda.pdf"
+            title="CV en PDF"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fa-solid fa-file"></i>
+          </a>
+          <p className="textCV">
+            ¡Haz click en los iconos para visitar los perfiles o descargar el
+            CV!
+          </p>
+        </div>
+      </main>
       <Footer />
     </>
   );
